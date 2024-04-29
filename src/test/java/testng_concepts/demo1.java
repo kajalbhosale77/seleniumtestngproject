c1725d7b330776ba2431fa0088493257b4cb79d3
kajalbhosale77 <kadamkajal1930@gmail.com>
2024-04-29 08:53:18 +0530

    first commit

---
+++ src/test/java/testng_concepts/demo1.java
@@ -0,0 +1,20 @@
+package testng_concepts;
+
+import org.testng.annotations.Test;
+
+public class demo1 {
+	
+	@Test
+	public void test1() {
+		 System.out.println("hii1");
+		
+	}
+	
+	@Test
+	
+	public void test2() {
+		System.out.println("hii2");
+	}
+	
+
+}
---
+++ src/test/java/testng_concepts/demo2.java
@@ -0,0 +1,17 @@
+package testng_concepts;
+
+import org.testng.annotations.Test;
+
+public class demo2 {
+
+	@Test
+	
+	public void test1() {
+		System.out.println("hii1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("hii2");
+	}
+}
